f7745b8fd25cbac9313e3e529313ad239776129b
Jose Jimenez-Luna <hawk31@gmail.com>
2025-02-26 07:21:09 -0800

    skip hpacker setup on CI

--- tests/test_mdrelax.py
+++ tests/test_mdrelax.py
@@ -26,8 +26,14 @@ def run_one_md_nointegration(
     return run_one_md(frame, only_energy_minimization=only_energy_minimization, simtime_ns=0.0)
 
 
+def _ensure_hpacker_mock():
+    """Skip hpacker install on CI"""
+    pass
+
+
 @patch("bioemu.sidechain_relax._run_hpacker", _run_hpacker_mock)
 @patch("bioemu.sidechain_relax.run_one_md", run_one_md_nointegration)
+@patch("bioemu.hpacker_setup.setup_hpacker.ensure_hpacker_install", _ensure_hpacker_mock)
 def test_mdrelax_integration(tmp_path):
     """integration test for md-relaxation pipeline
     assert that structure does not diverge too much from original sample
